92f20ecd95b76ad9e8c41d3dd809c753ded76785
Giselle Pacheco <gpacheco003@gmail.com>
2020-03-10 14:28:30 -0400

    added solution for union, added gitignore and babelrc

--- union/union.test.js
+++ union/union.test.js
@@ -4,5 +4,8 @@ describe('lodash/fp/union', () => {
   it('should merge arrays together', () => {
     const arrayOne = [1, 2, 3];
     const arrayTwo = [4, 5, 6];
+    const unionize = union(arrayTwo, arrayOne);
+
+    expect(unionize).toEqual([...new Set([...arrayOne, ...arrayTwo])])
   });
 });
